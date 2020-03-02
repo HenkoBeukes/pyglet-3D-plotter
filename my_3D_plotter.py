19f41dcc92a5d141a909f1f2fe0d8e1d254afdbe
Henko Beukes <henkobeukes@gmail.com>
2020-03-02 16:10:13 +0200

    Added copyright blurb

--- my_3D_plotter.py
+++ my_3D_plotter.py
@@ -1,4 +1,17 @@
 
+
+# Copyright (C) 2019 by Henko Beukes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
 """
 my pyglet based 3D plotter
 press p to see the info and input page
@@ -375,7 +388,7 @@ class TextWindow(pyglet.window.Window):
             window.equation.append(')')
         elif symbol == key.ASCIICIRCUM:
             window.equation.append('**')
-
+y
         elif symbol == key.Z:
             window.equation.append('z')
         elif symbol == key.X:
